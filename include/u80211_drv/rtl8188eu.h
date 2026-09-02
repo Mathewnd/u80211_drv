63856ceab2c30ae6283df0624a82a5680203e926
Matheus <matheusuky@gmail.com>
2026-09-02 02:52:17 +0100

    rtl8188eu: do not accept TRP_VAUX_EN chips and cut 8 chips

--- include/u80211_drv/rtl8188eu.h
+++ include/u80211_drv/rtl8188eu.h
@@ -6,6 +6,8 @@
 #include <u80211_drv/kernel_interface.h>
 
 #define U80211_DRV_RTL8188EU_REG_SYS_CFG 0xf0
+#define U80211_DRV_RTL8188EU_REG_SYS_CFG_TRP_VAUX_EN (1u << 23)
+#define U80211_DRV_RTL8188EU_REG_SYS_CFG_VER(value) (((value) >> 12) & 0xfu)
 
 int u80211_drv_rtl8188eu_reg_read8(u80211_drv_device_handle_t device, uint16_t reg, uint8_t *value);
 int u80211_drv_rtl8188eu_reg_read16(u80211_drv_device_handle_t device, uint16_t reg, uint16_t *value);
--- src/rtl8188eu/main.c
+++ src/rtl8188eu/main.c
@@ -4,6 +4,7 @@
 #include <u80211_drv/rtl8188eu.h>
 #include <u80211_drv/status.h>
 
+
 int u80211_drv_rtl8188eu_init(u80211_drv_device_handle_t device, u80211_drv_interface_handle_t interface) {
 	(void)interface;
 	uint32_t sys_cfg;
@@ -13,6 +14,8 @@ int u80211_drv_rtl8188eu_init(u80211_drv_device_handle_t device, u80211_drv_inte
 	if (status != U80211_DRV_STATUS_SUCCESS)
 		return status;
 
-	printf("REG_SYS_CFG: 0x%08x\n", sys_cfg);
+	if ((sys_cfg & U80211_DRV_RTL8188EU_REG_SYS_CFG_TRP_VAUX_EN) != 0 || U80211_DRV_RTL8188EU_REG_SYS_CFG_VER(sys_cfg) == 8)
+		return U80211_DRV_STATUS_NOT_SUPPORTED;
+
 	return U80211_DRV_STATUS_SUCCESS;
 }
